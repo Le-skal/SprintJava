c7e55ca2dd414a03ade5e64bd318e4d427bff7e9
Le-skal <raphael.martin.2004@gmail.com>
2025-02-04 17:36:27 +0100

    x

---
+++ Cours/CompteARebours.java
@@ -0,0 +1,9 @@
+package Cours;
+public class CompteARebours {
+    public static void main(String[] args) {
+        System.out.println("Lancement !");
+        for (int x = 10; x>0;x--){
+            System.out.println(x);
+        }
+    }
+}
---
+++ Cours/pairs.java
@@ -0,0 +1,10 @@
+package Cours;
+public class pairs {
+    public static void main(String[] args) {
+        for (int x = 0; x <= 20; x++) {
+            if (x % 2 == 0 && x != 0) {
+                System.out.println(x + " is even");
+            }
+        }
+    }
+}

02eecafc685d1bd9ddb8e5217c6bf726e72e1649
Le-skal <raphael.martin.2004@gmail.com>
2025-02-05 14:51:08 +0100

    objet

---
+++ Cours/exoObjet1/Main.java
@@ -0,0 +1,16 @@
+package Cours.exoObjet1;
+
+public class Main {
+    public static void main(String[] args) {
+        Voiture maVoiture = new Voiture();
+        
+        maVoiture.marque = "Toyota";
+        maVoiture.modele = "Picnic";
+        maVoiture.couleur = "decapotable";
+        
+        maVoiture.demarrer();
+        maVoiture.accelerer();
+        maVoiture.freiner();
+    }
+}
+

79c8671088a5827831942d93e8aaa8a7deeab892
Le-skal <raphael.martin.2004@gmail.com>
2025-02-11 14:20:40 +0100

    x

--- exoObjet2/Main.java
+++ exoObjet2/Main.java
@@ -1,5 +1,11 @@
 package exoObjet2;
 
 public class Main {
-    
+    public static void main(String[] args) {
+        Person p1 = new Person();
+        p1.sePresenter();
+
+        Person p2 = new Person("Alice", 30);
+        p2.sePresenter();
+    }
 }
--- exoObjet2/Person.java
+++ exoObjet2/Person.java
@@ -1,5 +1,20 @@
 package exoObjet2;
 
 public class Person {
-    
+    private String nom;
+    private int age;
+
+    public Person() {
+        this.nom = "Inconnu";
+        this.age = 0;
+    }
+
+    public Person(String nom, int age) {
+        this.nom = nom;
+        this.age = age;
+    }
+
+    public void sePresenter() {
+        System.out.println("Je m'appelle " + nom + " et j'ai " + age + " ans.");
+    }
 }
